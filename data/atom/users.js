c8f9a57b7b8c0a3f8c2e7da6c609bca342b71d70
Bendalam Bhargav <Bendalam Bhargav@edynamic.net>
2023-08-22 18:28:44 +0530

    Initial Project files

---
+++ data/atom/users.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, email: 'demo@altudo.co', password: 'Altudo@DeMo' },
+  { id: 2, email: 'test@altudo.co', password: '123456' },
+  // Add more dummy user data if needed
+];
+
+export default users;
